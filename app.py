ed5f57f5c33537fac792b8c7cff5484a0b84711b
iyadh-dach <iyadhdachraoui28@gmail.com>
2026-02-10 16:02:51 +0100

    refactor: Message de bienvenue mis à jour et titre principal

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-print('Bienvenue dans notre application collaborative !')
-print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
+print('Hello, world! Bienvenue à tous les collaborateurs !')
 print('Fonctionnalité de login ajoutée par Etudiant A.')
+print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
