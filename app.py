9aa9c187d4db8cd5601f6407f8c84cd9833e3159
aymn-bc <aymenbencheikh789@gmail.com>
2026-02-10 15:44:09 +0100

    feat: Implémentation du tableau de bord

--- app.py
+++ app.py
@@ -1,1 +1,2 @@
-"print('Bienvenue dans notre application collaborative !')" 
+print('Bienvenue dans notre application collaborative !')
+print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
